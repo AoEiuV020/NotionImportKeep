8b6f1735108ece47de0a46ef326975c1ae139d4f
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-25 01:12:41 +0800

    开启循环导入所有记事

--- main.py
+++ main.py
@@ -33,18 +33,18 @@ else:
     print('get database', block)
     co = client.get_block(block).collection
 
-json_name = list_file[-1]
-json_content = open(os.path.join(real_path, json_name), 'rb').read()
-sha256 = hashlib.sha256(json_content).hexdigest()
-print('import note from', repr(json_name), 'sha256', repr(sha256))
-jmap = json.loads(str(json_content, encoding='utf-8'))
-exists_rows = co.get_rows(search=sha256)
-if len(exists_rows):
-    print('exists row')
-    row = exists_rows[0]
-else:
-    print('create row')
-    row = co.add_row()
-util.import_keep_row(client, co, row, jmap, sha256)
-util.import_keep_cover(row, real_path, jmap)
-print('ok', repr(json_name), 'sha256', repr(sha256))
+for json_name in list_file:
+    json_content = open(os.path.join(real_path, json_name), 'rb').read()
+    sha256 = hashlib.sha256(json_content).hexdigest()
+    print('import note from', repr(json_name), 'sha256', repr(sha256))
+    jmap = json.loads(str(json_content, encoding='utf-8'))
+    exists_rows = co.get_rows(search=sha256)
+    if len(exists_rows):
+        print('exists row')
+        row = exists_rows[0]
+    else:
+        print('create row')
+        row = co.add_row()
+    util.import_keep_row(client, co, row, jmap, sha256)
+    util.import_keep_cover(row, real_path, jmap)
+    print('ok', repr(json_name), 'sha256', repr(sha256))
